dec6daf72153e8c93027de08072071fd4b2e52b4
Jonathan Steininger <Jonathan.steininger@icloud.com>
2024-07-15 10:09:52 +1200

    updated building scripts.

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,26 @@ using UnityEngine;
 public class Building
 {
     public string name;
-    public float cookiesPerSecond;
+    public float cookiesPerSecond; 
     public uint price;
-    private int boughtCount;
+    public int boughtCount; 
+
+    public float CookiesPerSecond { get { return cookiesPerSecond; } set { name = value; changed = true; } }
+    public uint Price { get { return price; } set { name = value; changed = true; } }
+    public string Name { get { return name; } set { name = value; changed = true; } }
+    public int BoughtCount { get { return boughtCount; } set { name = value; changed = true; } }
+
+
+    private bool changed = false;
+
+
+    public float totalCookiesPerSecond => cookiesPerSecond * boughtCount;
+
+    public bool hasChanged()
+    {
+
+
+    }
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/BuildingTile.cs
+++ Assets/Scripts/BuildingTile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -24,6 +25,20 @@ public class BuildingTile : MonoBehaviour
     private TextReference _CPSText;
     private TextReference _totalCPSText;
 
+
+    private Building buildingStats = null;
+
+    public Building getBuildingStats()
+    {
+        if (buildingStats == null)
+        {
+            return null;
+        }
+        return buildingStats;
+    }
+
+
+
     private class TextReference
     {
         public GameObject gameObject;
@@ -33,40 +48,91 @@ public class BuildingTile : MonoBehaviour
             gameObject = text;
             textComponent = gameObject.GetComponent<TMP_Text>();
         }
+
+        public void UpdateText(string text)
+        {
+            if (text == null)
+            {
+                Debug.Log("inputed Text missing");
+                return;
+            }
+            if (textComponent == null)
+            {
+                Debug.Log("Missing textComponent somehow. (Should not be possible (unless manually done))");
+                return;
+            }
+            textComponent.text = text;
+        }
     }
     private class ButtonReferences
     {
+        public GameObject gameObject;
         public Button buttonComponent;
-        public GameObject textObject;
         public TextReference textReference;
         public ButtonReferences(GameObject button)
         {
+            gameObject = button;
             buttonComponent = button.GetComponent<Button>();
-            textObject = button.transform.Find("Text (TMP)").gameObject;
+            textReference = new TextReference(button.transform.Find("Text (TMP)").gameObject);
+        }
+        public void UpdateText(string text)
+        {
+            if (textReference == null)
+            {
+                Debug.Log("missing textReference in button. Can't update text.");
+                return;
+            }
+            textReference.UpdateText(text);
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
-        
+        GetReferences();
     }
 
     private void GetReferences()
     {
+        Debug.Log("Loading BuildingTile References");
+        try
+        {
+            _buyButton = new ButtonReferences(buyButtonObject);
+            _sellButton = new ButtonReferences(sellButtonObject);
 
+            _nameText = new TextReference(nameTextObject);
+            _priceText = new TextReference(priceTextObject);
+            _countText = new TextReference(countTextObject);
+            _CPSText = new TextReference(CPSTextObject);
+            _totalCPSText = new TextReference(totalCPSTextObject);
+        } catch (Exception e)
+        {
+            Debug.Log($"Error loading BuildingTile references. : ${e.Message}");
+            throw e;
+        }
     }
 
 
     public void updateText()
     {
-
+        if (buildingStats == null)
+        {
+            Debug.Log("BuildingStats not declared. Skipping updating UI.");
+            return;
+        }
+        _nameText.UpdateText(buildingStats.name);
+        _priceText.UpdateText($"Price: ${buildingStats.price} Cookies");
+        _countText.UpdateText($"Count: ${buildingStats.boughtCount}");
+        _CPSText.UpdateText($"CPS: ${buildingStats.cookiesPerSecond}");
+        _totalCPSText.UpdateText($"Total CPS: ${buildingStats.totalCookiesPerSecond}");
+        // Could later make this change dynamically.
+        _buyButton.UpdateText("Buy");
+        _sellButton.UpdateText("Sell");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateText();
     }
 }
