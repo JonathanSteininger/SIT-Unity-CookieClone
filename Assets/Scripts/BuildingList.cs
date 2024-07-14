b452d34e46375c8eba19e8b7ed3754d3d17936aa
Jonathan Steininger <Jonathan.steininger@icloud.com>
2024-07-15 09:33:08 +1200

    Created BuildingList script and attached prefab object into scripts.

---
+++ Assets/Scripts/BuildingList.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefabBuildingTile;
+    private List<GameObject> buildingList = new List<GameObject>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public AddBuilding()
+    {
+
+    }
+}
